7e081954c15f3dcbdfeed210a0da8d6718d409f7
Arthur Souza Armond <asarmond82@gmail.com>
2025-05-01 16:31:01 -0300

    change lock variables

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -4,24 +4,21 @@ get_db_name(){
     print_banner
     printf "${WHITE} 🗓️ Insira o nome do Banco de Dados (Não utilizar caracteres especiais):${GRAY_LIGHT}"
     printf "\n\n"
-    #read -p "> " db_name
-    db_name="teste"
+    read -p "> " db_name
 }
 
 get_db_user(){
     print_banner
     printf "${WHITE} 🗓️ Insira o usuário do Banco de Dados (Não utilizar caracteres especiais):${GRAY_LIGHT}"
     printf "\n\n"
-    #read -p "> " db_user
-    db_user="teste_user"
+    read -p "> " db_user
 }
 
 get_db_password(){
     print_banner
     printf "${WHITE} 🗓️ Insira a senha do usuário ${db_user} do Banco de Dados (Não utilizar caracteres especiais):${GRAY_LIGHT}"
     printf "\n\n"
-    #read -p "> " db_password
-    db_password="123456"
+    read -p "> " db_password
 }
 
 get_db_info(){
@@ -40,32 +37,28 @@ get_link_git(){
     print_banner
     printf "${WHITE} 💻 Insira o link do GITHUB do repositório desejado: ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " link_git
-    link_git="https://github.com/weeblink/plataforma_paidoecommerce.git"
+    read -p "> " link_git
 }
 
 get_frontend_domain(){
     print_banner
     printf "${WHITE} 💻 Digite o domínio do FRONTEND/PAINEL para a ${plataform_name}:${GRAY_LIGHT}"
     printf "\n\n"
-    #read -p "> " frontend_url
-    frontend_url="app.paidoecommerce.com"
+    read -p "> " frontend_url
 }
 
 get_backend_domain(){
     print_banner
     printf "${WHITE} 💻 Digite o domínio do BACKEND/API para a ${plataform_name}:${GRAY_LIGHT}"
     printf "\n\n"
-    #read -p "> " backend_url
-    backend_url="api.paidoecommerce.com"
+    read -p "> " backend_url
 }
 
 get_whatsapp_service_domain(){
     print_banner
     printf "${WHITE} 💻 Digite o domínio do WHATSAPP SERVICE para a ${plataform_name}:${GRAY_LIGHT}"
     printf "\n\n"
-    #read -p "> " whatsappservice_url
-    whatsappservice_url="ws.paidoecommerce.com"
+    read -p "> " whatsappservice_url
 }
 
 get_domains(){
@@ -78,40 +71,35 @@ get_plataform_name(){
     print_banner
     printf "${WHITE} 📊 Digite o nome da plataforma que será instalada: ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " plataform_name
-    plataform_name="paidoecommerce"
+    read -p "> " plataform_name
 }
 
 get_frontend_port(){
     print_banner
     printf "${WHITE} 🧵 Digite a porta do FRONTEND ( 3000 a 3999 ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " frontend_port
-    frontend_port="3000"
+    read -p "> " frontend_port
 }
 
 get_backend_port(){
     print_banner
     printf "${WHITE} 🧵 Digite a porta do BACKEND ( 4000 a 4999 ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " backend_port
-    backend_port="4000"
+    read -p "> " backend_port
 }
 
 get_wsservice_port(){
     print_banner
     printf "${WHITE} 🧵 Digite a porta do WHATSAPP SERVICE ( 6000 a 6999 ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " wsservice_port
-    wsservice_port="6000"
+    read -p "> " wsservice_port
 }
 
 get_database_port(){
     print_banner
     printf "${WHITE} 🧵 Digite a porta do POSTGRESQL ( 5432 ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " postgresql_port
-    postgresql_port="5432"
+    read -p "> " postgresql_port
 }
 
 get_ports(){
@@ -125,32 +113,28 @@ get_mail_host(){
     print_banner
     printf "${WHITE} 🧵 Digite o host do email ( smtp.hostinger.com ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " mail_host
-    mail_host="smtp.hostinger.com"
+    read -p "> " mail_host
 }
 
 get_mail_port(){
     print_banner
     printf "${WHITE} 🧵 Digite a porta do servidor do email ( 465 ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " mail_port
-    mail_port="465"
+    read -p "> " mail_port
 }
 
 get_mail_username(){
     print_banner
     printf "${WHITE} 🧵 Digite o username do email ( username@${frontend_url} ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " mail_username
-    mail_username="naoresponda@paidoecommerce.com"
+    read -p "> " mail_username
 }
 
 get_mail_password(){
     print_banner
     printf "${WHITE} 🧵 Digite a senha do email ( ******* ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " mail_password
-    mail_password="7R|];hScT="
+    read -p "> " mail_password
 }
 
 get_mail_info(){
@@ -164,16 +148,14 @@ get_pandavideo_info(){
     print_banner
     printf "${WHITE} 🧵 Digite a credencial do PandaVideo ( panda-xxxxxxxxx ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " pandavideo_credential
-    pandavideo_credential="panda-fcfdcbb5f5ce4f0805311b5ecf99f0543b9696e4a2c369cf17d80f85548e057d"
+    read -p "> " pandavideo_credential
 }
 
 get_checkout_info(){
     print_banner
     printf "${WHITE} 🧵 Digite o IP permitido para receber comunicações do checkout de pagamento ( 15.229.103.46 ): ${GRAY_LIGHT}"
     printf "\n\n";
-    #read -p "> " checkoutapp_allowed_ip
-    checkoutapp_allowed_ip="15.229.103.46"
+    read -p "> " checkoutapp_allowed_ip
 }
 
 create_plataform(){
